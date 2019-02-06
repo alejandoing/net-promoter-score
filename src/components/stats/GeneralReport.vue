86bf0c5a39895e7233785196ed6fe8b74ade199b
Alejandro Uray <urayalejandro@gmail.com>
2019-02-06 14:17:32 -0300

    General Report Button

---
+++ src/components/stats/GeneralReport.vue
@@ -0,0 +1,14 @@
+<script>
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+export default {
+  created() {
+    var doc = new jsPDF()
+
+    doc.setFontSize(24)
+    doc.text('Reporte General de Valoraciones', 40, 25)
+    doc.save('reporte_general.pdf')
+  }
+}
+</script>
+
